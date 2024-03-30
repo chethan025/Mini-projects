370e3c718e805f27c28697dbffd7543f73d2b9d7
Chethan S <chethansc0965499@gmail.com>
2024-03-30 22:22:48 +0530

    Update game.js

--- Guessing Game/game.js
+++ Guessing Game/game.js
@@ -1,7 +1,7 @@
 var fhm= Math.floor(Math.random()*11)
             console.log(fhm);
             while(true){
-                var gnum=Number(prompt("enter a number"))
+                var gnum=Number(prompt("enter a number 1-10"))
                 if(gnum==fhm){
                     alert ("Correct")
                     
